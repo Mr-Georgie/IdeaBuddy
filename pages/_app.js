0cb88c77df5a652137c9ab17dc9a192b31e78d0c
George Isiguzo <George.isiguzo@yahoo.com>
2024-01-11 10:16:37 +0100

    added speed insights for vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { AppProvider } from "@/AppContext";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Outfit } from "next/font/google";
 import Head from "next/head";
 import "../styles/globals.css";
@@ -20,6 +21,7 @@ function App({ Component, pageProps }) {
                 <main className={`${outfit.variable} font-body`}>
                     <Component {...pageProps} />
                     <Analytics />
+                    <SpeedInsights />
                 </main>
             </UserProvider>
         </AppProvider>
